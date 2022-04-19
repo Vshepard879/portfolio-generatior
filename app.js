3c466caa85028fe85e3b5aa7608e8f51499c330b
Vshepard879 <vshepard879@gmail.com>
2022-04-19 17:06:54 -0400

    initiated inquirer

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
 
-const profileDataArgs = process.argv.slice(2);
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
 
-const [name, github] = profileDataArgs;
+  .then(answers => console.log(answers));
+// const fs = require('fs');
 
+// const generatePage = require('./src/page-template');
 
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
