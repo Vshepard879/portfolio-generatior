2a8af240531f34f42a898cef1c4cfade1601f30d
Vshepard879 <vshepard879@gmail.com>
2022-04-19 01:36:05 -0400

    cleaned up function with arrow and forEach

---
+++ app.js
@@ -0,0 +1,42 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = profileDataArr => {
+    //This...
+  
+
+    profileDataArr.forEach(profileItem =>console.log(profileItem));
+
+
+    
+};
+
+printProfileData(profileDataArgs);
